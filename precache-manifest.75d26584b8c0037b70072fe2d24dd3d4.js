71cb873a9a87745df88417672fa41096d43abf05
Jorge Ayala Saenz <jorgeayalasaenz0391@gmail.com>
2019-09-06 22:56:51 -0500

    Updates

---
+++ precache-manifest.75d26584b8c0037b70072fe2d24dd3d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "003a7e922e727cfbd685",
+    "url": "/miCV/static/js/runtime~main.003a7e92.js"
+  },
+  {
+    "revision": "46dca24f399d70b872ca",
+    "url": "/miCV/static/js/main.46dca24f.chunk.js"
+  },
+  {
+    "revision": "9634f9b5a4e696353e77",
+    "url": "/miCV/static/js/2.9634f9b5.chunk.js"
+  },
+  {
+    "revision": "46dca24f399d70b872ca",
+    "url": "/miCV/static/css/main.26b48730.chunk.css"
+  },
+  {
+    "revision": "3cf93ba089b16c8e92a6feec4a5711cf",
+    "url": "/miCV/index.html"
+  }
+];
